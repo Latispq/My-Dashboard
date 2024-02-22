a4e7169bc223fca36610d50a142e99402b87ae30
Latispq <Latispq@gmail.com>
2024-02-22 01:09:58 -0500

    added data cards

--- src/components/GridView.jsx
+++ src/components/GridView.jsx
@@ -3,6 +3,7 @@ import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
+import OutlinedCard from './DataCard';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -16,17 +17,20 @@ export default function RowAndColumnSpacing() {
   return (
     <Box sx={{ width: '100%' }}>
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid item xs={6}>
-          <Item>1</Item>
+        <Grid item xs={4}>
+          <Item><OutlinedCard /></Item>
         </Grid>
-        <Grid item xs={6}>
-          <Item>2</Item>
+        <Grid item xs={4}>
+          <Item><OutlinedCard /></Item>
         </Grid>
-        <Grid item xs={6}>
-          <Item>3</Item>
+        <Grid item xs={4}>
+          <Item><OutlinedCard /></Item>
         </Grid>
-        <Grid item xs={6}>
-          <Item>4</Item>
+        <Grid item xs={8}>
+          <Item><OutlinedCard /></Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item><OutlinedCard /></Item>
         </Grid>
       </Grid>
     </Box>
